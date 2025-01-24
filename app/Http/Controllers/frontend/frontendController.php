85ee2f750708807c4575d6970bc8da641cdf05fc
AnodMohamed <Anodmohamed2020@gmail.com>
2025-01-24 21:05:55 +0300

    index About

--- app/Http/Controllers/frontend/frontendController.php
+++ app/Http/Controllers/frontend/frontendController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Education;
 use App\Models\Info;
+use App\Models\Profile;
 use Illuminate\Http\Request;
 
 class frontendController extends Controller
@@ -11,7 +13,10 @@ class frontendController extends Controller
     //
     public function index(){
         $info = Info::where('user_id', 2)->first();
-        return view('index', compact('info'));
+        $profile = Profile::where('user_id',2)->first();
+        $edu = Education::where('user_id',2)->first();
+
+        return view('index', compact('info','profile','edu'));
 
     }
 }
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -179,74 +179,7 @@
         },
         placeholder: "___.___.___.___"
       });
-    // editor
-    var editor = document.getElementById('editor');
-    if (editor) {
-      var toolbarOptions = [
-        [
-          {
-            'font': []
-          }],
-        [
-          {
-            'header': [1, 2, 3, 4, 5, 6, false]
-          }],
-        ['bold', 'italic', 'underline', 'strike'],
-        ['blockquote', 'code-block'],
-        [
-          {
-            'header': 1
-          },
-          {
-            'header': 2
-          }],
-        [
-          {
-            'list': 'ordered'
-          },
-          {
-            'list': 'bullet'
-          }],
-        [
-          {
-            'script': 'sub'
-          },
-          {
-            'script': 'super'
-          }],
-        [
-          {
-            'indent': '-1'
-          },
-          {
-            'indent': '+1'
-          }], // outdent/indent
-        [
-          {
-            'direction': 'rtl'
-          }], // text direction
-        [
-          {
-            'color': []
-          },
-          {
-            'background': []
-          }], // dropdown with defaults from theme
-        [
-          {
-            'align': []
-          }],
-        ['clean'] // remove formatting button
-      ];
-      var quill = new Quill(editor,
-        {
-          modules:
-          {
-            toolbar: toolbarOptions
-          },
-          theme: 'snow'
-        });
-    }
+    
     // Example starter JavaScript for disabling form submissions if there are invalid fields
     (function () {
       'use strict';
